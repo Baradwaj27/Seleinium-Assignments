02daa3ff0f702bb7ce40f1cfc183f148cb3e9aac
Baradwaj27 <126316161+Baradwaj27@users.noreply.github.com>
2025-02-01 05:54:32 -0800

    Add files via upload
    Week1.Day1

---
+++ week1/day1/DataTypes.java
@@ -0,0 +1,38 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) 
+	{
+		byte age=24;
+		
+		short salaryOfMe=20000;
+		
+		int amount=50000;
+		
+		long mobileNumber=9080072461l;
+		
+		float rate= 1.12345769f;
+		
+		double rate1=7.123456789;
+		
+		char initial='M', initial2='R';
+		
+		boolean doYouKnowJava = true;
+		
+		String name = "Baradwaj";
+		
+		
+		System.out.println(name);
+		System.out.println(initial);
+		System.out.println(initial2);		
+		System.out.println(age);
+		System.out.println(salaryOfMe);
+		System.out.println(mobileNumber);
+		System.out.println(amount);
+		System.out.println(rate);
+		System.out.println(rate1);
+		System.out.println(doYouKnowJava);
+	}
+
+}
