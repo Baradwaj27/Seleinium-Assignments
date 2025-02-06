967e26bb61ae3a6fc272c71966a7f12262c5447d
Baradwaj27 <126316161+Baradwaj27@users.noreply.github.com>
2025-02-06 18:45:32 +0530

    Add files via upload

---
+++ selenium/FirstSelenium.java
@@ -0,0 +1,11 @@
+package selenium;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class FirstSelenium {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		}
+
+
+}
